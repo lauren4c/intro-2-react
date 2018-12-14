2959862abe8c6abc02198f478aa62ab8dfc457d0
Lauren Forcey <lauren4c@gmail.com>
2018-12-14 07:59:49 -0700

    Checkpoint 2752 Add Events Completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,11 +4,11 @@
  	 render() {
      return (
        <li>
-       	<input type="checkbox" checked={ this.props.isCompleted } />
+       	<input type="checkbox" checked={ this.props.isCompleted } onChange= {this.props.toggleComplete }/>
       	 <span>{ this.props.description }</span>
        </li>
      );
    }
  }
- 
+
   export default ToDo;
